81efa4c761563b72649fd72033ffed06f76b0bd7
vikas <vickybgoudar@gmail.com>
2024-02-07 20:47:57 +0530

    fixed the init sql script

--- sql/init.sql
+++ sql/init.sql
@@ -1,11 +1,20 @@
-
-CREATE EXTENSION IF NOT EXISTS citext; 
-
 CREATE DATABASE pynemonk_core;
 
 \c pynemonk_core;
 
+CREATE EXTENSION IF NOT EXISTS citext; 
+
 CREATE SCHEMA IF NOT EXISTS auth;
+
+CREATE TABLE pynemonk_core.auth.role (
+    id int NOT NULL, 
+    name int NOT NULL,
+    description text,
+    is_deleted BOOLEAN DEFAULT FALSE, 
+    created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY(id)
+); 
 
 CREATE TABLE pynemonk_core.auth.user (
     id int NOT NULL, 
@@ -13,6 +22,21 @@ CREATE TABLE pynemonk_core.auth.user (
     role_id int NOT NULL, 
     is_deleted BOOLEAN DEFAULT FALSE, 
     created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
-    updated_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
+    updated_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY (id),
+    CONSTRAINT fk_user_role_id_ref_role_id FOREIGN KEY (role_id) REFERENCES pynemonk_core.auth.role(id)
 );
-  
+
+CREATE TABLE pynemonk_core.auth.user_profile (
+    id int NOT NULL, 
+    user_id int NOT NULL,
+    first_name varchar,
+    last_name varchar,
+    phone varchar, 
+    date_of_birth DATE, 
+    is_deleted BOOLEAN DEFAULT FALSE, 
+    created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY(id),
+    CONSTRAINT fk_user_profile_user_id_ref_user_id FOREIGN KEY (user_id) REFERENCES pynemonk_core.auth.user(id)
+);
