552ccce0640472ff854ed2eb5f39975a72cf8b13
Vikas Biradargoudar <vickybgoudar@gmail.com>
2026-04-20 17:00:20 +0530

    Increamental changes (#10)
    * added new changes
    
    * t
    
    * PG support for ssl and connection string
    
    ---------
    
    Co-authored-by: Vikas Biradargoudar <vikas.biradargoudar@gmail.com>

--- services/pynemonk-core-school/src/db/pg-pool.ts
+++ services/pynemonk-core-school/src/db/pg-pool.ts
@@ -1,6 +1,7 @@
 import pkg from "pg";
 const { Pool } = pkg;
 const pool = new Pool({
+    connectionString: process.env.DATABASE_URL,
     host: process.env.PGHOST || "localhost",
     port: parseInt(process.env.PGPORT || "5432"),
     user: process.env.PGUSER || "postgres",
@@ -8,7 +9,7 @@ const pool = new Pool({
     database: process.env.PGDATABASE || "pynemonk_core",
     max: 20,
     idleTimeoutMillis: 30000,
-    connectionTimeoutMillis: 2000,
+    connectionTimeoutMillis: 10000,
 });
 
 export default pool;
